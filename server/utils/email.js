bedd0e3d96bc40a5eaea18bdcfb82c90d09fe4bb
NamanAgarwal <naman.ag41402@gmail.com>
2022-02-06 15:23:06 +0530

    added email functionality

---
+++ server/utils/email.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (user, subject) => {
+  let to = user.email;
+
+  let transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER_ADDRESS,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  let mailOptions = {
+    from: `Client Connect <${process.env.USER_ADDRESS}>`,
+    to,
+    subject: subject.title,
+    html,
+  };
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log("Email sent successfully!");
+  });
+};
+
+module.exports = sendEmail;
