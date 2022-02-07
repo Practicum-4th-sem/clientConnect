d7706157c1b9ca8fd67ef2118e3bf163a919d21b
NamanAgarwal <naman.ag41402@gmail.com>
2022-02-07 16:30:41 +0530

    sms setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
-
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/users", userRoutes);
 
