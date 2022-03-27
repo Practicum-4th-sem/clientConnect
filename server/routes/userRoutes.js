9752b801182d2e573124d9b4dd89b2059f4e2d74
NamanAgarwal <naman.ag41402@gmail.com>
2022-03-27 12:29:43 +0530

    fixed some bugs

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/verifyOtp", authController.verifyPhoneOtp);
+router.post("/verifyOtp", authController.verifyOtp);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword", authController.resetPassword);
 
