1b83ad2826e9b46d8d5d63e838c53392b10f7fcd
Satwik-pal-git <satwikpal01@gmail.com>
2022-04-08 23:17:05 +0530

    authcheck done

--- server/routes/mainroutes.js
+++ server/routes/mainroutes.js
@@ -16,24 +16,12 @@ router.get("/", (req, res) => {
 });
 
 router.get("/dashboard", authcheck, (req, res) => {
-    // console.log(req.user);
     const UserDetails = req.user;
-    res.render("dashboard", { naam: UserDetails.username, gmail: UserDetails.Gmail, photo: UserDetails.image });
+    res.render("dashboard", { UserDetails, naam: UserDetails.name, gmail: UserDetails.email, pic: UserDetails.photo, phone: UserDetails.phone });
 });
 
-router.post("/register", (req, res) => {
-    console.log("Register", req);
-});
 
-router.post("/login", (req, res) => {
-    console.log("Login", req);
-});
-
-router.get("/login", (req, res) => {
-    res.render("login");
-});
-
-router.get("/profile", authcheck, (req, res) => {
+router.get("/api/v1/users/profile", authcheck, (req, res) => {
     res.render("profile", { User: req.user.username, Image: req.user.image });
 });
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,20 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", authController.register);
+
+router.get("/register", (req, res) => {
+    console.log("this is the register");
+    res.render("langing_page");
+    //rendering the landing page for trial....
+});
+
 router.post("/login", authController.login);
+
+router.get("/login", (req, res) => {
+    console.log("this is the login");
+    res.render("langing_page");
+});
+
 router.post("/verifyOtp", authController.verifyOTP);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword", authController.resetPassword);
