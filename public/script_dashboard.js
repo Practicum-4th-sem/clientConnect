87e5671f50e71a127a8396b455acfde3d2f35dba
gunjan1909 <bhanarkaranita@gmail.com>
2022-04-23 22:53:04 +0530

    fixes

--- public/script_dashboard.js
+++ public/script_dashboard.js
@@ -14,3 +14,64 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+//SCRIPT FOR SLIDERS IN DASHBOARD
+// SLIDER
+window.onload = function () {
+  $(".slider").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+    speed: 4000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    // dots: true,
+    // infinite: true,
+    // speed: 300,
+    // slidesToShow: 4,
+    // slidesToScroll: 4,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
+  });
+};
