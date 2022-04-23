43f3811e7ed055ac5f695766832f3828fe8a9f34
gunjan1909 <bhanarkaranita@gmail.com>
2022-04-24 00:45:24 +0530

    form templates, favicon in ejs files: login form wip

--- static frontend/dashboard/script.js
+++ static frontend/dashboard/script.js
@@ -1,1 +1,16 @@
-//ABI KUCH NAI HAI IDHR
+// add hovered class in selected list item
+let list = document.querySelectorAll(".navigation li");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("hovered"));
+  this.classList.add("hovered");
+}
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+//MENU TOGGLER
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
