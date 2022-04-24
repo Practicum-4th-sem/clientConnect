4b695ec5d485b84f9cd740f4d37dbbc1f96e130f
gunjan1909 <bhanarkaranita@gmail.com>
2022-04-24 19:10:33 +0530

    login form progress

--- static frontend/login form multistep/multi_step_form_script.js
+++ static frontend/login form multistep/multi_step_form_script.js
@@ -14,7 +14,7 @@ function showTab(n) {
   if (n == x.length - 1) {
     document.getElementById("nextBtn").innerHTML = "Submit";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    //document.getElementById("nextBtn").innerHTML = "Next";
   }
   // ... and run a function that displays the correct step indicator:
   fixStepIndicator(n);
