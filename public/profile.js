527ab8d9d6322d46ba585e83d61ace9594b8beb1
NamanAgarwal <naman.ag41402@gmail.com>
2022-04-27 16:25:50 +0530

    updating profile details done

--- public/profile.js
+++ public/profile.js
@@ -1,12 +1,6 @@
-// const axios = require("axios");
 let data = {};
 function changeHandler(key, value) {
   console.log(key, value);
   data[key] = value;
   console.log(data);
-}
-
-async function submitHandler() {
-  const res = await axios.patch("/updateProfile", data);
-  console.log(res.data);
 }
