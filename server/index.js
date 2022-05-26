72e701a5cbf8fc36a228c42ac950b5e841ef7a11
NamanAgarwal <naman.ag41402@gmail.com>
2022-05-26 15:11:32 +0530

    fixed bug

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const app = require('./app');
-const connectDb = require('./mongo/mongo');
+const app = require("./app");
+const connectDb = require("./mongo/mongo");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 connectDb();
 app.listen(port, () => {
-    console.log(`Server started on ${port}`);
-})
+  console.log(`Server started on ${port}`);
+});
